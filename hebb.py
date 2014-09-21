1251edbb6235211b735a7fa9c99dc8341ba24b7a
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-09-21 19:04:30 -0400

    Hebb Rule program from HW2

---
+++ hebb.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python2
+
+#16 different binary logic functions: http://en.wikipedia.org/wiki/Boolean_algebras_canonically_defined#Truth_tables
+#x0	x1	2f0	2f1	2f2	2f3	2f4	2f5	2f6	2f7	2f8	2f9	2f10	wf11	2f12	2f13	2f14	2f15
+#F	F	F	T	F	T	F	T	F	T	F	T	F	T	F	T	F	T
+#T	F	F	F	T	T	F	F	T	T	F	F	T	T	F	F	T	T
+#F	T	F	F	F	F	T	T	T	T	F	F	F	F	T	T	T	T
+#T	T	F	F	F	F	F	F	F	F	T	T	T	T	T	T	T	T
+
+
