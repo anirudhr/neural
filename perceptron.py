9304981c72eb20386736b74d7094eb1a9fb7c055
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-09-26 15:24:07 -0400

    Transfer func

--- perceptron.py
+++ perceptron.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python2
+
+class classifier:
+    def __init__(self, weights, threshold):
+        self.weights = weights
+        self.threshold = threshold
+    def transfer(self, x):
+        if x > self.threshold:
+            return 1
+        elif x < -1 * self.threshold:
+            return -1
+        else:
+            return 0
+
