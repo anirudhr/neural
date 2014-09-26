4db749f50f104f24c8b4e4553a937fbcaaae263f
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-09-26 15:46:10 -0400

    Added transfer function, output calculation

--- perceptron.py
+++ perceptron.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python2
 
-class classifier:
-    def __init__(self, weights, threshold):
-        self.weights = weights
+class perceptron:
+    def __init__(self, w_vec, threshold):
+        self.w_vec = w_vec
         self.threshold = threshold
-    def transfer(self, x):
-        if x > self.threshold:
+        self.bias = -1 * threshold
+    def transfer(self, xx):
+        if xx > self.threshold:
             return 1
-        elif x < -1 * self.threshold:
+        elif xx < -1 * self.threshold:
             return -1
         else:
             return 0
-
+    def calc_yy(self, x_vec):
+        if len(x_vec) != len(self.w_vec):
+            raise Exception('Supplied input length does not match weight length.')
+        yy = self.bias
+        for xx,ww in zip(w_vec, self.w_vec):
+            yy += xx*ww
+        return yy
+    def train(self, s_vec, t_vec):
+        ;
