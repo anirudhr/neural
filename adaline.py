c33b142286b6d212c71573bbedb4f411cc3849b4
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-10-16 16:43:49 -0400

    continuing

--- adaline.py
+++ adaline.py
@@ -30,34 +30,7 @@ class perceptron:
             raise Exception('Rate not positive.')
         if len(s_vec_list) != len(t_vec):
             raise Exception('Training set problem: input count does not match result count.')
-        iter_count = 0
-        w_change_count = 0
-        max_w_change = len(s_vec_list)
-        while w_change_count < max_w_change:   #Step
-            for s_vec, tt in zip(s_vec_list, t_vec):
-                yin = self.calc_yin(s_vec)
-                yy = self.transfer(yin)
-                w_change = list()
-                for i in range(len(self.w_vec)):
-                    w_change.append(0)
-                bias_change = 0
-                if yy != tt:
-                    for ii, ww in enumerate(self.w_vec):
-                        w_change[ii] = rate*tt*s_vec[ii]
-                        self.w_vec[ii] = ww + w_change[ii]
-                    bias_change = rate*tt
-                    #print '***', bias_change, '***'
-                    self.bias += bias_change
-                    #self.threshold = -1 * self.bias
-                    w_change_count = 0
-                else:
-                    w_change_count += 1
-                #print '***', w_change_count, '***'
-                iter_count += 1
-                print iter_count, ': ', str(s_vec), '|', yin, '|', yy, '|', tt, '|', str(w_change), bias_change, str(self.w_vec), '|', self.bias #'***', w_change_count, '***', 
-                #time.sleep(1)
-                if w_change_count == max_w_change:
-                    break
+        
 ###
 
 p = perceptron([0, 0, 0], 0.1, 0)
