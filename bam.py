2f861a8ccfb7c04c959c13cb266b52211184ea9b
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-25 11:51:30 -0500

    working!

--- bam.py
+++ bam.py
@@ -2,6 +2,7 @@
 #:indentSize=4:tabSize=4:noTabs=true:wrap=soft:
 
 import numpy as np
+import re
 
 def simple_transfer(x):
         if x > 0:
@@ -15,14 +16,14 @@ class BAM:
     def __init__(self, s_mat_list, t_mat_list): #s_mat_list, t_mat_list = list of np.matrix
         self.transfer = np.vectorize(simple_transfer) #transfer function
         self.setweight(s_mat_list, t_mat_list)
-    def setweight(s_mat_list, t_mat_list):
-        self.w_mat = np.empty([s_mat_list[0].shape[1], t_mat_list[0].shape[1]]) #will be filled with garbage values, will be same as using np.zeros() but marginally faster
+    def setweight(self, s_mat_list, t_mat_list):
+        self.w_mat = np.matrix(np.empty([s_mat_list[0].shape[1], t_mat_list[0].shape[1]])) #will be filled with garbage values, will be same as using np.zeros() but marginally faster
         for s_mat, t_mat in zip(s_mat_list, t_mat_list):
             self.w_mat += s_mat.getT() * t_mat
-    def inp_left(x_mat):
+    def inp_left(self, x_mat):
         return self.transfer(x_mat * self.w_mat)
     
-    def inp_left(y_mat):
+    def inp_right(self, y_mat):
         return self.transfer(y_mat * self.w_mat.getT())
     
 def translate_input(inputtxt): #converts a string such as '.##\n#..\n#..\n#..\n.##' into the input matrix
@@ -51,3 +52,7 @@ t_x = np.matrix('1 -1 1')
 
 inp_list = [translate_input(inp_c), translate_input(inp_d), translate_input(inp_x)]
 out_list = [t_c, t_d, t_x]
+
+bam_cdx = BAM(inp_list, out_list)
+print bam_cdx.inp_left(translate_input(inp_c))
+print bam_cdx.inp_right(t_d)
